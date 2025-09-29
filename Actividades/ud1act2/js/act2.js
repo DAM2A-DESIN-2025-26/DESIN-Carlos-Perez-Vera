1673858315247392e5d25814661ed8ac335c07d7
Carlos <carlospv18@educastur.es>
2025-09-29 11:11:09 +0200

    ud1ej24

--- Actividades/ud1act2/js/act2.js
+++ Actividades/ud1act2/js/act2.js
@@ -4,27 +4,35 @@ var jugador2 =  prompt("Introduce nombre de jugador: ");
 var opciones =["Piedra","Papel","Tijera"];
 
 
-var eleccion1 = prompt(jugador1+"Introduce tu eleccion");
-var eleccion2 = prompt(jugador2+"Introduce tu eleccion");
+var eleccion1 = prompt(jugador1+": Introduce tu eleccion");
+var eleccion2 = prompt(jugador2+": Introduce tu eleccion");
 var ganador=null;
 
 eleccion1 = eleccion1.charAt(0).toUpperCase() + eleccion1.slice(1).toLowerCase();
 eleccion2 = eleccion2.charAt(0).toUpperCase() + eleccion2.slice(1).toLowerCase();
 
 
-if (opciones.includes(eleccion1) || opciones.includes(eleccion2)){
+/* if (opciones.includes(eleccion1) === opciones.includes(eleccion2)){
     alert ("Elija una respuesta valida");
-}else {
+}else { */
   if (eleccion1 === eleccion2) {
     alert("¡Habéis empatado!");
   } else if (
-    (eleccion1 === "Piedra" && eleccion2 === "Tijera") ||(eleccion1 === "Papel" && eleccion2 === "Piedra") ||(eleccion1 === "Tijera" && eleccion2 === "Papel")
-  ) {
+    (eleccion1 === "Piedra" && eleccion2 === "Tijera") ||
+    (eleccion1 === "Papel" && eleccion2 === "Piedra") ||
+    (eleccion1 === "Tijera" && eleccion2 === "Papel"))
+     {
     alert("El ganador es " + jugador1);
-  } else {
+    
+  } else if((eleccion2 === "Piedra" && eleccion1 === "Tijera") ||
+    (eleccion2 === "Papel" && eleccion1 === "Piedra") ||
+    (eleccion2 === "Tijera" && eleccion1 === "Papel")) {
+
     alert("El ganador es " + jugador2);
+  }else{
+    alert("Elija una respuesta valida");
   }
-}
+
 
 /* switch(ganador){
     case opcion1:
