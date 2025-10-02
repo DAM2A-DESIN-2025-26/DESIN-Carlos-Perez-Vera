5aae607ea3e1f4859841972e586aeaa5299526a4
Carlos <carlospv18@educastur.es>
2025-10-02 10:30:38 +0200

    ud1ej6

--- Actividades/ud1act6/js/ej6.js
+++ Actividades/ud1act6/js/ej6.js
@@ -1,19 +1,44 @@
+const numero = parseInt(prompt("Introduzca el dinero que quiere convertir..."));
 
+function valorABilletes(numero) {
+    let sobra = numero;
 
-const numero=prompt("Introduzca el dinero que quiere convertir...");
+    let Nbilletes500 = Math.floor(sobra / 500);
+    sobra = sobra % 500;
 
+    let Nbilletes200 = Math.floor(sobra / 200);
+    sobra = sobra % 200;
 
-function billetesDe500(numero,sobra){
-    let Nbilletes500=0;
-     for(let i=numero;i<500;i++){
-        let Nbilletes500=numero/500;
-        let sobra500 =numero%500;
-    }
-    
-alert(
-        cantidad + "€ equivale a " + 
-        Nbilletes500 + " billetes de 500€ y sobran " + sobra + "€."
-    );    
+    let Nbilletes100 = Math.floor(sobra / 100);
+    sobra = sobra % 100;
+
+    let Nbilletes50 = Math.floor(sobra / 50);
+    sobra = sobra % 50;
+
+    let Nbilletes20 = Math.floor(sobra / 20);
+    sobra = sobra % 20;
+
+    let Nbilletes10 = Math.floor(sobra / 10);
+    sobra = sobra % 10;
+
+    let Nbilletes5 = Math.floor(sobra / 5);
+    sobra = sobra % 5;
+
+    alert(
+        "El numero de billetes de 500 es: " + Nbilletes500 + "\n" +
+        "El numero de billetes de 200 es: " + Nbilletes200 + "\n" +
+        "El numero de billetes de 100 es: " + Nbilletes100 + "\n" +
+        "El numero de billetes de 50 es: " + Nbilletes50 + "\n" +
+        "El numero de billetes de 20 es: " + Nbilletes20 + "\n" +
+        "El numero de billetes de 10 es: " + Nbilletes10 + "\n" +
+        "El numero de billetes de 5 es: " + Nbilletes5 + "\n" +
+        "Sobra sin convertir: " + sobra
+    );
 }
 
-billetesDe500( numero,sobra);
+valorABilletes(numero);
+
+
+
+
+ 
