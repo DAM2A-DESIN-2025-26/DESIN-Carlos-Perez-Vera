86ebed03fb12c5b70aebc86808ee25b7d5c02bbd
Carlos Pérez <carlospv8@educastur.es>
2025-10-06 19:30:17 +0200

    Ud1_Act2

---
+++ Actividades/Ud1_Act2/js/ej1.js
@@ -0,0 +1,20 @@
+/* 1. Crea un script que pida al usuario su nombre y apellidos y muestre:*/
+
+let nombre="";
+let apellidos="";
+let nombreUsuario ="";
+let contraseña="";
+
+/* El tamaño del nombre más los apellidos (sin contar espacios).*/
+
+/* La cadena en minúsculas y en mayúsculas.*/
+
+/* Que divida el nombre y los apellidos y los muestre en 3 líneas, donde ponga:*/
+
+
+/* Una propuesta de nombre de usuario, compuesto por el nombre, la inicial del*/
+/* primer apellido y la inicial del segundo: ej. Para Javier Gonzalez Pisano sería*/
+/* javiergp. Implementa esta funcionalidad mediante una función anónima.*/
+/* Una propuesta de nombre de usuario compuesto por las dos primeras letras del*/
+/* nombre y de los dos apellidos: ej. jagopi. Implementa esta funcionalidad*/
+/* mediante una función flecha. */
