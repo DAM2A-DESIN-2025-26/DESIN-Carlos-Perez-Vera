ff6c99c0eee5fb2d8d9bb8af01d4350b9cfc61f3
CarlosPV-oss <carlospv18@educastur.es>
2025-10-08 19:08:43 +0200

    ej2ACt2

---
+++ Actividades/Ud1_Act2/js/ej2.js
@@ -0,0 +1,11 @@
+
+let dia=0;
+
+let mes=0;
+
+var diasSemana= ["Lunes","Martes","Miercoles","Viernes","Sabado","Domingo"]
+
+
+function infoCumple(){
+    
+}
---
+++ Ejemplos/ud1/ejmplo2.js
@@ -0,0 +1,14 @@
+/* var diasLaborables=new Array();
+diasLaborables[0]=new Array("Lunes","Martes","Miercoles","Jueves","Viernes");
+diasLaborables[1]=new Array("Monday","Tuesday","Wednesday","Thursday","Friday");
+diasLaborables[2]=new Array("Lundi","Mardi","Mercredi","Jeudi","Vendredi");
+diasLaborables[3]=new Array("Montag","Dienstag","Mittwoch","Donnerstag","Freitag");
+console.log("La semana empieza en "+diasLaborables[0][0]);
+console.log("Week ends on "+diasLaborables[1][4]);
+
+for(let i = 0;i<diasLaborables.length;i++){
+    for(let index = 0;index<diasLaborables[i].length;index++);
+        const element = 
+
+    }
+ */
