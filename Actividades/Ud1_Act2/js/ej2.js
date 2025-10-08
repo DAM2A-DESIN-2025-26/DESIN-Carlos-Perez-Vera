0b734084cc7936672f74822053d3e20c75b693bf
CarlosPV-oss <carlospv18@educastur.es>
2025-10-08 20:12:17 +0200

    ej2_v1

--- Actividades/Ud1_Act2/js/ej2.js
+++ Actividades/Ud1_Act2/js/ej2.js
@@ -1,11 +1,33 @@
 
-let dia=0;
 
-let mes=0;
+var diasSemana= ["Lunes","Martes","Miercoles","Viernes","Sabado","Domingo"];
+var meses= ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
 
-var diasSemana= ["Lunes","Martes","Miercoles","Viernes","Sabado","Domingo"]
+Date();
 
 
 function infoCumple(){
+
+    var dia =parseInt( prompt("Que dia es hoy? "));
+    var mes =parseInt( prompt("De que mes?"));
+
+    var hoy = new Date();
+    var anioActual = hoy.getFullYear;
+
+    var cumpleaños = new Date(anioActual,mes-1,dia);
+
+    if (cumpleaños<hoy){
+        cumpleaños = new Date(anioActual +1,mes-1,dia);
+    }
+
+    var miliSegs = cumpleaños-hoy;
+    var dias = Math.ceil(miliSegs / (1000 * 60 * 60 * 24));
+    
+    var diasSemana = diasSemana[cumpleaños.getDay()]
+
+    
     
 }
+
+
+infoCumple();
