7d744be2f2eac4ffd2f76cbd60196a3a22f13ebf
CarlosPV-oss <carlospv18@educastur.es>
2025-11-12 20:05:28 +0100

    cmm

--- Ud2/ejemplos/ejemplo1.1/js/Biblioteca.js
+++ Ud2/ejemplos/ejemplo1.1/js/Biblioteca.js
@@ -1,3 +1,66 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+/* const libro1 = {
+    nombre: "El Hobbit",
+    color: "Rojo",
+    autor: "Tolkien",
+    nPaginas: 300,
+    editorial: "Minotauro",
+    forrado: false,
+    coverURL: "wadwadgd",
+    comprar: function () {
+        return `Libro de aventuras del autor ${autor} comprado`;
+    }
+};
+
+const libro2 = {
+    nombre: "El Quijote",
+    color: "Azul",
+    autor: "Cervantes",
+    nPaginas: 500,
+    editorial: "Planeta",
+    forrado: true,
+    coverURL: "gesdfesf",
+    comprar: function () {
+        return `Libro de aventuras del autor ${autor} comprado`;
+    }
+};
+
+const libro3 = {
+    nombre: "1984",
+    color: "Verde",
+    autor: "George Orwell",
+    nPaginas: 600,
+    editorial: "Planeta",
+    forrado: false,
+    coverURL: "adwwafaw",
+    comprar: function () {
+        return `Libro de aventuras del autor ${autor} comprado`;
+    }
+};
+
+const libro4 = {
+    nombre: "El Lazarillo de Tormes",
+    color: "Gris",
+    autor: "Anónimo",
+    nPaginas: 100,
+    editorial: "Ariel",
+    forrado: true,
+    coverURL: "wfadfwwaf",
+    comprar: function () {
+        return `Libro de aventuras del autor ${autor} comprado`;
+    }
+};
+
+let biblioteca = [libro1, libro2, libro3];
+
+const checkPages = ({ nPaginas }) => nPaginas > 150;
+
+const checkLibro = (libro, biblioteca) => biblioteca.includes(libro);
+
+console.log(checkPages(libro1));
+console.log(checkPages(libro4));
+
+console.log(checkLibro(libro2, biblioteca));
+console.log(checkLibro(libro4, biblioteca)); */ 
 //# sourceMappingURL=Biblioteca.js.map
