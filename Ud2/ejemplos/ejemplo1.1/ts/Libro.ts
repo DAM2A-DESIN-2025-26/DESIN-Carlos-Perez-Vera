2e843d49eb2f525fbfe95a047daa7ebc4926425c
CarlosPV-oss <carlospv18@educastur.es>
2025-11-12 20:05:09 +0100

    ejemplo biblioteca

---
+++ Ud2/ejemplos/ejemplo1.1/ts/Libro.ts
@@ -0,0 +1,10 @@
+export type Libro = {
+    nombre : string;
+    color : string;
+    autor : string;
+    nPaginas : number;
+    editorial : string;
+    comprar(): void;
+    forrado : boolean;
+    coverURL : string;
+}
