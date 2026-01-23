a290cef550896d79d5015dd2170b6231f6e338b8
CarlosPV-oss <carlospv18@educastur.es>
2026-01-23 11:31:03 +0100

    Correccion de clases y app.route.ts

--- Ud4/gamehub/src/app/app.component.ts
+++ Ud4/gamehub/src/app/app.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { 
+  IonApp, 
+  IonMenu, 
+  IonContent, 
+  IonList, 
+  IonMenuToggle, 
+  IonItem, 
+  IonIcon, 
+  IonLabel, 
+  IonRouterOutlet 
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [
+    IonApp,
+    IonMenu,
+    IonContent,
+    IonList,
+    IonMenuToggle,
+    IonItem,
+    IonIcon,
+    IonLabel,
+    IonRouterOutlet
+  ],
+  standalone: true
 })
 export class AppComponent {
-  constructor() {}
+  abrirAcercaDe() {
+    console.log('Acerca de GameHub - modal pendiente');
+  }
 }
--- Ud4/gamehub/src/app/app.routes.ts
+++ Ud4/gamehub/src/app/app.routes.ts
@@ -3,34 +3,49 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    redirectTo: '/dashboard/juegos',
+    pathMatch: 'full',
   },
   {
     path: 'dashboard',
-    loadComponent: () => import('./dashboard/dashboard.page').then( m => m.DashboardPage)
+    loadComponent: () => import('./dashboard/dashboard.page').then(m => m.DashboardPage),
+    children: [
+      {
+        path: 'juegos',
+        loadComponent: () => import('./dashboard/juegos/juegos.page').then(m => m.JuegosPage)
+      },
+      {
+        path: 'tienda',
+        loadComponent: () => import('./dashboard/tienda/tienda.page').then(m => m.TiendaPage)
+      },
+      {
+        path: 'perfil',
+        loadComponent: () => import('./dashboard/perfil/perfil.page').then(m => m.PerfilPage),
+        children: [
+          {
+            path: 'configuracion',
+            loadComponent: () => import('./dashboard/perfil/configuracion/configuracion.page').then(m => m.ConfiguracionPage)
+          },
+          {
+            path: 'estadisticas',
+            loadComponent: () => import('./dashboard/perfil/estadisticas/estadisticas.page').then(m => m.EstadisticasPage)
+          },
+          {
+            path: '',
+            redirectTo: 'configuracion',
+            pathMatch: 'full',
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: 'juegos',
+        pathMatch: 'full',
+      }
+    ]
   },
   {
-    path: 'juegos',
-    loadComponent: () => import('./dashboard/juegos/juegos.page').then( m => m.JuegosPage)
-  },
-  {
-    path: 'tienda',
-    loadComponent: () => import('./dashboard/tienda/tienda.page').then( m => m.TiendaPage)
-  },
-  {
-    path: 'perfil',
-    loadComponent: () => import('./dashboard/perfil/perfil.page').then( m => m.PerfilPage)
-  },
-  {
-    path: 'juego-detalle',
-    loadComponent: () => import('./juego-detalle/juego-detalle.page').then( m => m.JuegoDetallePage)
-  },
-  {
-    path: 'configuracion',
-    loadComponent: () => import('./perfil/configuracion/configuracion.page').then( m => m.ConfiguracionPage)
-  },
-  {
-    path: 'estadisticas',
-    loadComponent: () => import('./perfil/estadisticas/estadisticas.page').then( m => m.EstadisticasPage)
+    path: 'juego-detalle/:id',
+    loadComponent: () => import('./juego-detalle/juego-detalle.page').then(m => m.JuegoDetallePage)
   },
 ];
