02c603d5b8f08a8c095a5a34c96c0e01b454d52e
CarlosPV-oss <carlospv18@educastur.es>
2026-01-26 18:22:57 +0100

    cambios en dashboard

--- Ud4/gamehub/src/app/dashboard/dashboard.page.ts
+++ Ud4/gamehub/src/app/dashboard/dashboard.page.ts
@@ -1,24 +1,13 @@
 import { Component } from '@angular/core';
-import { 
-  IonHeader, 
-  IonToolbar, 
-  IonTitle, 
-  IonContent, 
-  IonButtons, 
-  IonMenuButton 
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent,
+  IonButtons, IonMenuButton
 } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   standalone: true,
-  imports: [
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonButtons,
-    IonMenuButton
-  ]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton]
 })
-export class DashboardPage { }
+export class DashboardPage {}
